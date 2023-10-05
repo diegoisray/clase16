9523eb2b9c1460b110b15faccd870ba30bac7849
davisleon <davidaarondeleon@gmail.com>
2023-10-05 20:58:28 -0300

    se modifico el archivo app.js

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const path = require ("path")
 app.listen(3030,()=>{
     console.log("servidor en linea")
 })
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/"))
+})
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/babbage.html"))
+})
+
+
